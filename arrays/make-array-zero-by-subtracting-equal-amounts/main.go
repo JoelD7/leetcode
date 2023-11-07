6f08d3706dd2b0a8b01061ca59c35092ca7e932b
Joel Fabian <jfabian@truora.com>
2023-11-07 19:23:14 -0400

    Solve problem: make array zero by subtracting equal amounts

---
+++ arrays/make-array-zero-by-subtracting-equal-amounts/main.go
@@ -0,0 +1,44 @@
+package make_array_zero_by_subtracting_equal_amounts
+
+import "sort"
+
+func minimumOperations(nums []int) int {
+	//
+	//
+	//
+	//
+	//
+	x := 0
+	operationsCount := 0
+
+	for {
+		sort.Ints(nums)
+
+		for _, val := range nums {
+			if val != 0 && val > 1 {
+				x = val - 1
+			}
+
+			if val != 0 {
+				x = val
+			}
+		}
+
+		if x == 0 && operationsCount == 0 {
+			return 0
+		}
+
+		if x == 0 {
+			return operationsCount
+		}
+
+		for i, val := range nums {
+			if val != 0 {
+				nums[i] = val - x
+			}
+		}
+
+		operationsCount++
+		x = 0
+	}
+}
