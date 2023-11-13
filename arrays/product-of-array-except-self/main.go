17f975b029ac049a959e57b5b694936c6e830d97
Joel Fabian <jfabian@truora.com>
2023-11-13 16:26:01 -0400

    Add prefixProduct and suffixProduct functions

--- arrays/product-of-array-except-self/main.go
+++ arrays/product-of-array-except-self/main.go
@@ -1,9 +1,13 @@
 package product_of_array_except_self
 
+import "fmt"
+
 // Input: nums = [1,2,3,4]
 // Output: [24,12,8,6]
 func productExceptSelf(nums []int) []int {
 	result := make([]int, 0)
+
+	fmt.Println(prefixProduct(nums))
 
 	for i := 0; i < len(nums); i++ {
 		val := 1
@@ -16,6 +20,40 @@ func productExceptSelf(nums []int) []int {
 		}
 
 		result = append(result, val)
+	}
+
+	return result
+}
+
+// input: [1,2,3,4]
+// output: [1,2,6,24]
+func prefixProduct(nums []int) []int {
+	prev := nums[0]
+	result := make([]int, 0, len(nums))
+	result = append(result, prev)
+
+	for i := 1; i < len(nums); i++ {
+		result = append(result, prev*nums[i])
+		prev = prev * nums[i]
+	}
+
+	return result
+}
+
+// input: [1,2,3,4]
+// output: [24,24,12,4]
+func suffixProduct(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	next := nums[len(nums)-1]
+	result := make([]int, len(nums))
+	result[len(nums)-1] = next
+
+	for i := len(nums) - 2; i >= 0; i-- {
+		result[i] = next * nums[i]
+		next = next * nums[i]
 	}
 
 	return result
--- arrays/product-of-array-except-self/main_test.go
+++ arrays/product-of-array-except-self/main_test.go
@@ -9,9 +9,32 @@ func TestProductExceptSelf(t *testing.T) {
 	c := require.New(t)
 
 	result := productExceptSelf([]int{1, 2, 3, 4})
+
 	c.Len(result, 4)
 	c.Equal(24, result[0])
 	c.Equal(12, result[1])
 	c.Equal(8, result[2])
 	c.Equal(6, result[3])
+}
+
+func TestPrefixProduct(t *testing.T) {
+	c := require.New(t)
+
+	result := prefixProduct([]int{1, 2, 3, 4})
+	c.Len(result, 4)
+	c.Equal(1, result[0])
+	c.Equal(2, result[1])
+	c.Equal(6, result[2])
+	c.Equal(24, result[3])
+}
+
+func TestSuffixProduct(t *testing.T) {
+	c := require.New(t)
+
+	result := suffixProduct([]int{1, 2, 3, 4})
+	c.Len(result, 4)
+	c.Equal(24, result[0])
+	c.Equal(24, result[1])
+	c.Equal(12, result[2])
+	c.Equal(4, result[3])
 }
