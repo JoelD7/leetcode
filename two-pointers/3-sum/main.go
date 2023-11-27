7867747335bcda2e35e8eb776c56443f13dab1ab
Joel Fabian <jfabian@truora.com>
2023-11-27 19:38:57 -0400

    First iteration three sum

--- two-pointers/3-sum/main.go
+++ two-pointers/3-sum/main.go
@@ -1,1 +1,56 @@
-package __sum
+package three_sum
+
+func threeSum(nums []int) [][]int {
+	//prefixSum := getPrefixSum(nums)
+	triplets := make([][]int, 0)
+
+	var l, r int
+
+	//{-1, 0, 1, 2, -1, -4}
+	for i := 0; i < len(nums); i++ {
+		l = 0
+		r = len(nums) - 1
+
+		for l < r {
+			if l == i {
+				l++
+				continue
+			}
+
+			if r == i {
+				r--
+				continue
+			}
+
+			if nums[i]+nums[l]+nums[r] == 0 {
+				triplets = append(triplets, []int{nums[i], nums[l], nums[r]})
+			}
+
+			l++
+			r--
+		}
+	}
+
+	return triplets
+}
+
+func getPrefixSum(nums []int) []int {
+	prev := nums[0]
+	ps := make([]int, 0)
+	ps = append(ps, prev)
+
+	for i := 1; i < len(nums); i++ {
+		ps = append(ps, prev+nums[i])
+		prev += nums[i]
+	}
+
+	return ps
+}
+
+func rangeSum(l, r int, prefixSum []int) int {
+	if l == 0 {
+		return prefixSum[r]
+	}
+
+	return prefixSum[r] - prefixSum[l-1]
+}
--- two-pointers/3-sum/main_test.go
+++ two-pointers/3-sum/main_test.go
@@ -1,1 +1,13 @@
 package three_sum
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	//c := require.New(t)
+
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(nums))
+}
