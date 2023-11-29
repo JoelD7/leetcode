b0f487cdb1d6c7c23b01aa89947bbdb5062203b8
Joel Fabian <jfabian@truora.com>
2023-11-28 21:16:34 -0400

    Start implementation of Binary Search for three sum

--- two-pointers/3-sum/main.go
+++ two-pointers/3-sum/main.go
@@ -9,21 +9,38 @@ func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
 	triplets := make([][]int, 0)
 
-	var l, r, cur, sum int
+	var l, r, m, cur, lSum, rSum int
 	var tripletStr string
 	var ok bool
 	addedTriplets := make(map[string]struct{})
 
+	//-4 -1 -1 0 1 2
 	for i := 0; i < len(nums)-1; i++ {
 		cur = nums[i]
 		l = i + 1
 		r = len(nums) - 1
+		m = (l + r) / 2
+
+		lSum = cur + nums[l] + nums[m]
+		rSum = cur + nums[r] + nums[m]
+
+		if lSum < 0 {
+			l = m
+			m = r
+		} else if lSum == 0 {
+			tripletStr = fmt.Sprintf("%d%d%d", cur, nums[l], nums[r])
+
+			_, ok = addedTriplets[tripletStr]
+			if !ok {
+				triplets = append(triplets, []int{cur, nums[l], nums[r]})
+
+				addedTriplets[tripletStr] = struct{}{}
+			}
+		}
 
 		for l < r {
 
-			sum = cur + nums[l] + nums[r]
-
-			if sum == 0 {
+			if lSum == 0 {
 				tripletStr = fmt.Sprintf("%d%d%d", cur, nums[l], nums[r])
 
 				_, ok = addedTriplets[tripletStr]
@@ -33,7 +50,7 @@ func threeSum(nums []int) [][]int {
 					addedTriplets[tripletStr] = struct{}{}
 				}
 				l++
-			} else if sum > 0 {
+			} else if lSum > 0 {
 				r--
 			} else {
 				l++
