14cd5284b22d1a4ffdba909b46591a6e30323747
Author Name <joeld200013@gmail.com>
2023-12-27 18:36:21 -0400

    Daily temperatures: start implementation using stack

--- stack/daily-temperatures/main.go
+++ stack/daily-temperatures/main.go
@@ -1,16 +1,87 @@
 package daily_temperatures
 
+// [73,74,75,71,69,72,76,73]
 func dailyTemperatures(temperatures []int) []int {
 	result := make([]int, len(temperatures))
 
-	for i, temp := range temperatures {
-		for j := i + 1; j < len(temperatures); j++ {
-			if temperatures[j] > temp {
-				result[i] = j - i
+	stack := newStack()
+
+	var cur, next, top, popCount, j int
+	for i := 0; i < len(temperatures)-1; i++ {
+		cur = temperatures[i]
+		next = temperatures[i+1]
+
+		if next > cur {
+			result[i] = 1
+			continue
+		}
+
+		if stack.length > 0 {
+			for {
+				top = stack.pop()
+				popCount++
+
+				if top > cur {
+
+				}
+
+				if top == cur {
+					break
+				}
+			}
+
+			result[i] = popCount
+			continue
+		}
+
+		j = i + 1
+		for {
+			next = temperatures[j]
+			if next > cur {
+				result[i] = stack.length + 1
 				break
 			}
+
+			stack.push(next)
+			j++
 		}
 	}
 
 	return result
+}
+
+type node struct {
+	val  int
+	prev *node
+}
+
+type Stack struct {
+	head   *node
+	length int
+}
+
+func newStack() *Stack {
+	return &Stack{nil, 0}
+}
+
+func (s *Stack) push(val int) {
+	s.length++
+
+	if s.head == nil || s.length == 0 {
+		s.head = &node{val, nil}
+		return
+	}
+
+	curHead := &node{s.head.val, s.head.prev}
+	s.head.val = val
+	s.head.prev = curHead
+}
+
+func (s *Stack) pop() int {
+	s.length--
+
+	curHead := &node{s.head.val, s.head.prev}
+	s.head = curHead.prev
+
+	return curHead.val
 }
