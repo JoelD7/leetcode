6c7a41aaa99e57d3fbd72de75402032230b9a314
JoelD7 <joeld200013@gmail.com>
2025-07-18 19:24:19 -0400

    Attempt to solve "3sum"
    This passes 309 / 314 testcases

--- main.go
+++ main.go
@@ -1,47 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
+	fmt.Printf("Expected: [[-1,-1,2],[-1,0,1]], Got: %+v\n", threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	fmt.Printf("Expected: [[0,0,0]], Got: %+v\n", threeSum([]int{0, 0, 0, 0}))
 }
 
-func productExceptSelf(nums []int) []int {
-	result := make([]int, len(nums))
-	prefix := prefixProduct(nums)
-	sufix := sufixProduct(nums)
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	l := 0
+	r := len(nums) - 1
+	k := 0
+	twoSum := 0 //Sum of the first two numbers of the triplet
+	opposite := 0
+	tripletMap := make(map[string]struct{})
+	var tripletStr string
 
-	for i := 0; i < len(nums); i++ {
-		if i == 0 {
-			result[i] = sufix[i+1]
-			continue
+	result := make([][]int, 0)
+
+	for l < r-1 {
+		for ; l < r-1; l++ {
+			//If the current left is the same as the previous one, we have already compared this value against the rest
+			//of the array. Doing it again will produce duplicate triplets. Example: [-4,-1,-1,0,1,2]
+			if l > 0 && nums[l] == nums[l-1] {
+				continue
+			}
+
+			twoSum = nums[l] + nums[r]
+
+			k = r - 1
+			opposite = twoSum * -1
+			for nums[k] >= opposite && k > l {
+				tripletStr = fmt.Sprintf("%d%d%d", nums[l], nums[r], nums[k])
+				if _, ok := tripletMap[tripletStr]; nums[k] == opposite && l != r && l != k && k != r && !ok {
+					result = append(result, []int{nums[l], nums[r], nums[k]})
+					tripletMap[tripletStr] = struct{}{}
+					break
+				}
+				k--
+			}
 		}
-
-		if i == len(nums)-1 {
-			result[i] = prefix[i-1]
-			continue
-		}
-
-		result[i] = prefix[i-1] * sufix[i+1]
-	}
-
-	return result
-}
-
-func prefixProduct(nums []int) []int {
-	result := make([]int, len(nums))
-	result[0] = nums[0]
-
-	for i := 1; i < len(nums); i++ {
-		result[i] = result[i-1] * nums[i]
-	}
-
-	return result
-}
-
-func sufixProduct(nums []int) []int {
-	result := make([]int, len(nums))
-	result[len(nums)-1] = nums[len(nums)-1]
-
-	for i := len(nums) - 2; i >= 0; i-- {
-		result[i] = result[i+1] * nums[i]
+		l = 0
+		r--
 	}
 
 	return result
