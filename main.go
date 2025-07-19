db25d3c24f52183cddc34ea3db7f7752ce9f0e9a
JoelD7 <joeld200013@gmail.com>
2025-07-19 10:18:33 -0400

    Solve "3sum"
    This passes all tests cases but it only beats 8.76% of solutions. I have to improve it

--- main.go
+++ main.go
@@ -10,19 +10,34 @@ func main() {
 	fmt.Printf("Expected: [[0,0,0]], Got: %+v\n", threeSum([]int{0, 0, 0, 0}))
 }
 
+// -1,0,1,2,-1,-4
+// -4,-1,-1,0,1,2
 func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
 	l := 0
 	r := len(nums) - 1
-	k := 0
 	twoSum := 0 //Sum of the first two numbers of the triplet
 	opposite := 0
 	tripletMap := make(map[string]struct{})
 	var tripletStr string
+	posByNums := make(map[int][]int)
+
+	if nums[l] == nums[r] && nums[l] == 0 {
+		return [][]int{nums[:3]}
+	}
+
+	for i, num := range nums {
+		if pos, ok := posByNums[num]; ok {
+			posByNums[num] = append(pos, i)
+			continue
+		}
+
+		posByNums[num] = []int{i}
+	}
 
 	result := make([][]int, 0)
 
-	for l < r-1 {
+	for ; l < r-1; r-- {
 		for ; l < r-1; l++ {
 			//If the current left is the same as the previous one, we have already compared this value against the rest
 			//of the array. Doing it again will produce duplicate triplets. Example: [-4,-1,-1,0,1,2]
@@ -32,20 +47,22 @@ func threeSum(nums []int) [][]int {
 
 			twoSum = nums[l] + nums[r]
 
-			k = r - 1
 			opposite = twoSum * -1
-			for nums[k] >= opposite && k > l {
-				tripletStr = fmt.Sprintf("%d%d%d", nums[l], nums[r], nums[k])
-				if _, ok := tripletMap[tripletStr]; nums[k] == opposite && l != r && l != k && k != r && !ok {
-					result = append(result, []int{nums[l], nums[r], nums[k]})
+			pos, ok := posByNums[opposite]
+			if !ok {
+				continue
+			}
+
+			for _, p := range pos {
+				tripletStr = fmt.Sprintf("%d%d%d", nums[l], nums[r], nums[p])
+				if _, ok := tripletMap[tripletStr]; l < p && r > p && !ok {
+					result = append(result, []int{nums[l], nums[r], nums[p]})
 					tripletMap[tripletStr] = struct{}{}
 					break
 				}
-				k--
 			}
 		}
 		l = 0
-		r--
 	}
 
 	return result
