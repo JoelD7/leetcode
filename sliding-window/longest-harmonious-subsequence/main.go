f07613b4dc19a6131b817767d14ea9e029f93872
JoelD7 <joeld200013@gmail.com>
2025-07-24 19:59:31 -0400

    Solve "longest harmonious subsequence"

--- sliding-window/longest-harmonious-subsequence/main.go
+++ sliding-window/longest-harmonious-subsequence/main.go
@@ -13,6 +13,14 @@ func findLHS(nums []int) int {
 
 		return b
 	}
+	updateLargest := func(largest, count int) int {
+		//If count == 0, this means all items are equal
+		if count > 0 {
+			return max(largest, count+1)
+		}
+
+		return largest
+	}
 
 	var i, newI, diff, count, largest int
 	j := i + 1
@@ -21,17 +29,30 @@ func findLHS(nums []int) int {
 		diff = nums[j] - nums[i]
 		if diff == 0 {
 			j++
+			//I don't increase `count` here because all the items of `nums` might be equal
 			continue
 		}
 
-		if diff == 1 && newI == 0 {
+		if (diff == 1 || diff == -1) && newI == 0 {
 			newI = j
+			count = j - i //I'm only increasing `count` when I know there are different items((diff == 1 || diff==-1))
 			j++
-			count++
 			continue
 		}
 
-		if diff == 1 {
+		//This means that the first elements cannot be part of the LHS, so we ignore them and start forming our window from the next ones.
+		if (diff > 1 || diff < -1) && newI == 0 {
+			newI = j
+			i = newI
+			j = i + 1
+			continue
+		}
+
+		if diff == 1 || diff == -1 {
+			if nums[j-1] != nums[j] {
+				newI = j
+			}
+
 			if count == 0 {
 				count = j - i
 			} else {
@@ -42,8 +63,8 @@ func findLHS(nums []int) int {
 			continue
 		}
 
-		if diff > 1 {
-			largest = max(largest, count+1)
+		if diff > 1 || diff < -1 {
+			largest = updateLargest(largest, count)
 			count = 0
 			i = newI
 			newI = j
@@ -51,6 +72,8 @@ func findLHS(nums []int) int {
 			continue
 		}
 	}
+
+	largest = updateLargest(largest, count)
 
 	return largest
 }
