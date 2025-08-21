b0ad7bc151a7e01eeb04eb120fe5c90f432285d9
JoelD7 <joeld200013@gmail.com>
2025-08-20 20:28:42 -0400

    Solve "LRU-Cache" - non-optimal solution
    This implementation passes all tests cases on Leetcode, but I believe it can be improved

--- hash-table/lru-cache/main.go
+++ hash-table/lru-cache/main.go
@@ -1,22 +1,28 @@
 package lru_cache
 
+import (
+	"math"
+)
+
 type LRUCache struct {
-	ageCount int
-	oldest   int
-	cache    map[int]int
-	ageToKey map[int]int
-	cap      int
-	length   int
+	incr       int
+	lowestIncr int
+	cache      map[int]int
+	keyByIncr  map[int]int
+	incrByKey  map[int]int
+	cap        int
+	length     int
 }
 
 func Constructor(capacity int) LRUCache {
 	return LRUCache{
-		ageCount: 0,
-		length:   0,
-		oldest:   0,
-		cap:      capacity,
-		cache:    make(map[int]int),
-		ageToKey: make(map[int]int),
+		incr:       0,
+		lowestIncr: math.MaxInt32,
+		length:     0,
+		cap:        capacity,
+		cache:      make(map[int]int),
+		keyByIncr:  make(map[int]int),
+		incrByKey:  make(map[int]int),
 	}
 }
 
@@ -26,20 +32,57 @@ func (this *LRUCache) Get(key int) int {
 		return -1
 	}
 
+	var keyIncr int
+	keyIncr, ok = this.incrByKey[key]
+	if ok {
+		if keyIncr < this.lowestIncr {
+			this.lowestIncr = keyIncr + 1
+		}
+		delete(this.keyByIncr, keyIncr)
+	}
+
+	this.incr++
+	this.keyByIncr[this.incr] = key
+	this.incrByKey[key] = this.incr
 	return val
 }
 
 func (this *LRUCache) Put(key int, value int) {
+	var ok bool
+	_, ok = this.cache[key]
+	if ok {
+		this.cache[key] = value
+		this.incr++
+
+		cur, _ := this.incrByKey[key]
+		delete(this.keyByIncr, cur)
+		this.keyByIncr[this.incr] = key
+
+		this.incrByKey[key] = this.incr
+		return
+	}
+
 	if this.length == this.cap {
-		delete(this.cache, this.oldest)
-		this.oldest++
-		this.length = 0
+		ok = false
+		var k int
+		for !ok {
+			k, ok = this.keyByIncr[this.lowestIncr]
+			this.lowestIncr++
+		}
+		delete(this.cache, k)
+		delete(this.incrByKey, k)
 	}
 
 	this.cache[key] = value
-	this.ageToKey[this.ageCount] = key
-	this.ageCount++
-	this.length++
+	this.incr++
+	this.keyByIncr[this.incr] = key
+	if this.incr < this.lowestIncr {
+		this.lowestIncr = this.incr
+	}
+	this.incrByKey[key] = this.incr
+	if this.length < this.cap {
+		this.length++
+	}
 }
 
 /**
--- hash-table/lru-cache/main_test.go
+++ hash-table/lru-cache/main_test.go
@@ -10,14 +10,28 @@ import (
 func TestLRUCache(t *testing.T) {
 	c := require.New(t)
 
-	o := Constructor(2)
-	o.Put(1, 1)
-	o.Put(2, 2)
-	c.Equal(1, o.Get(1))
-	o.Put(3, 3)
-	c.Equal(-1, o.Get(2))
-	o.Put(4, 4)
-	c.Equal(-1, o.Get(1))
-	c.Equal(3, o.Get(3))
-	c.Equal(4, o.Get(4))
+	t.Run("Example 1", func(t *testing.T) {
+		o := Constructor(2)
+		o.Put(1, 1)
+		o.Put(2, 2)
+		c.Equal(1, o.Get(1))
+		o.Put(3, 3)
+		c.Equal(-1, o.Get(2))
+		o.Put(4, 4)
+		c.Equal(-1, o.Get(1))
+		c.Equal(3, o.Get(3))
+		c.Equal(4, o.Get(4))
+	})
+
+	//["LRUCache","put","put","put","put","get","get"]
+	//[[2],[2,1],[1,1],[2,3],[4,1],[1],[2]]
+	t.Run("Test case 10", func(t *testing.T) {
+		o := Constructor(2)
+		o.Put(2, 1)
+		o.Put(1, 1)
+		o.Put(2, 3)
+		o.Put(4, 1)
+		c.Equal(-1, o.Get(1))
+		c.Equal(3, o.Get(2))
+	})
 }
