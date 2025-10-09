34a7db103120cc90abaf5193445ace8a7c939415
JoelD7 <joeld200013@gmail.com>
2025-10-09 09:42:09 -0400

    [Minimum Size Subarray Sum]: Solve

--- arrays/sliding-window/min-size-subarray-sum/main.go
+++ arrays/sliding-window/min-size-subarray-sum/main.go
@@ -4,11 +4,7 @@ import (
 	"math"
 )
 
-// target = 11, nums = [1,2,3,4,5]
-//
-//	prefixSum =       [1,3,6,10,15]
 func minSubArrayLen(target int, nums []int) int {
-	prefix := prefixSum(nums)
 	min := func(a, b int) int {
 		if a < b {
 			return a
@@ -16,26 +12,15 @@ func minSubArrayLen(target int, nums []int) int {
 
 		return b
 	}
-	var i, j, sum int
+	var sum, i int
 	minLength := math.MaxInt32
 
-	if prefix[len(prefix)-1] < target {
-		return 0
-	}
+	for j := 0; j < len(nums); j++ {
+		sum += nums[j]
 
-	for ; j < len(prefix); j++ {
-		//All the numbers after this condition is satisfied for the first time are >= target
-		if prefix[j] >= target && i == 0 {
-			minLength = min(minLength, j+1)
-			i = j + 1
-			break
-		}
-	}
-
-	for ; i < len(nums); i++ {
-		sum += nums[i]
-		if sum >= target {
-			minLength = min(minLength, i-j)
+		for sum >= target {
+			minLength = min(minLength, j-i+1)
+			sum -= nums[i]
 			i++
 		}
 	}
@@ -45,15 +30,4 @@ func minSubArrayLen(target int, nums []int) int {
 	}
 
 	return minLength
-}
-
-func prefixSum(nums []int) []int {
-	prefix := make([]int, len(nums))
-	prefix[0] = nums[0]
-
-	for i := 1; i < len(nums); i++ {
-		prefix[i] = nums[i] + prefix[i-1]
-	}
-
-	return prefix
 }
