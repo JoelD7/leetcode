6dea2e1d7276dca4cfa6bfc44108dec6a6f1b698
JoelD7 <joeld200013@gmail.com>
2025-11-08 11:38:44 -0400

    [Combination sum 2] Attempt using a map to keep track of the added combinations
    - I get a "time limit exceeded" error with this.
    - Passes 172/176 test cases

--- recursion/combination-sum-2/main.go
+++ recursion/combination-sum-2/main.go
@@ -1,18 +1,32 @@
 package combination_sum_2
 
+import (
+	"sort"
+	"strconv"
+)
+
 func combinationSum2(candidates []int, target int) [][]int {
 	res := make([][]int, 0)
 	comb := make([]int, 0)
+	combsMade := make(map[string]struct{})
+	combKey := ""
 
-	res = backtrack(0, target, 0, candidates, comb, res)
+	sort.Ints(candidates)
+
+	res = backtrack(0, target, 0, candidates, comb, combKey, combsMade, res)
 
 	return res
 }
 
-func backtrack(start, target, sum int, candidates, comb []int, res [][]int) [][]int {
+func backtrack(start, target, sum int, candidates, comb []int, combKey string, combsMade map[string]struct{}, res [][]int) [][]int {
 	if sum == target {
+		if _, ok := combsMade[combKey]; ok {
+			return res
+		}
+
 		c := make([]int, len(comb))
 		copy(c, comb)
+		combsMade[combKey] = struct{}{}
 		res = append(res, c)
 		return res
 	}
@@ -24,12 +38,19 @@ func backtrack(start, target, sum int, candidates, comb []int, res [][]int) [][]
 	for i := start; i < len(candidates); i++ {
 		comb = append(comb, candidates[i])
 		sum += candidates[i]
+		combKey += strconv.Itoa(candidates[i])
 
-		res = backtrack(i+1, target, sum, candidates, comb, res)
+		res = backtrack(i+1, target, sum, candidates, comb, combKey, combsMade, res)
 
 		comb = comb[:len(comb)-1]
 		sum -= candidates[i]
+		combKey = backtrackCombKey(combKey, candidates[i])
 	}
 
 	return res
+}
+
+func backtrackCombKey(combKey string, candidate int) string {
+	candidateStr := strconv.Itoa(candidate)
+	return combKey[:len(combKey)-len(candidateStr)]
 }
