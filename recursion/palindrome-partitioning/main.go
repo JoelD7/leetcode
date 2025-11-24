6200495148bb21e214285216ff9f6a62524fb796
JoelD7 <joeld200013@gmail.com>
2025-11-24 10:09:14 -0400

    [Palindrom substring] Solved

--- recursion/palindrome-partitioning/main.go
+++ recursion/palindrome-partitioning/main.go
@@ -4,21 +4,21 @@ func partition(s string) [][]string {
 	comb := make([]string, 0)
 	res := make([][]string, 0)
 
-	return backtrack(0, s, comb, res)
+	return backtrack(s, comb, res)
 }
 
-func backtrack(start int, s string, comb []string, res [][]string) [][]string {
-	if start == len(s) {
+func backtrack(s string, comb []string, res [][]string) [][]string {
+	if s == "" {
 		c := make([]string, len(comb))
 		copy(c, comb)
 		res = append(res, c)
 		return res
 	}
 
-	for i := start; i < len(s); i++ {
-		if isPalindrome(s[:i+1]) {
-			comb = append(comb, s[:i+1])
-			res = backtrack(i+1, s, comb, res)
+	for i := 1; i <= len(s); i++ {
+		if isPalindrome(s[:i]) {
+			comb = append(comb, s[:i])
+			res = backtrack(s[i:], comb, res)
 			comb = comb[:len(comb)-1]
 		}
 	}
