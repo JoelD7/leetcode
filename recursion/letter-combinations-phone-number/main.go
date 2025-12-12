f16fef1974c11af2feda4c5c348adda9ca9e623f
JoelD7 <joeld200013@gmail.com>
2025-12-12 09:40:13 -0400

    [Letter combinations of a phone number] Attempt #2 - solved

--- recursion/letter-combinations-phone-number/main.go
+++ recursion/letter-combinations-phone-number/main.go
@@ -1,28 +1,34 @@
 package letter_combinations_phone_number
 
 func letterCombinations(digits string) []string {
-	ln := map[byte]string{
-		'2': "abc", '3': "def", '4': "ghi", '5': "jkl",
-		'6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz",
+	digitToLetters := map[byte][]string{
+		'2': {"a", "b", "c"},
+		'3': {"d", "e", "f"},
+		'4': {"g", "h", "i"},
+		'5': {"j", "k", "l"},
+		'6': {"m", "n", "o"},
+		'7': {"p", "q", "r", "s"},
+		'8': {"t", "u", "v"},
+		'9': {"w", "x", "y", "z"},
 	}
 
 	res := make([]string, 0)
 
-	var backtrack func(index int, digits, comb string)
+	var backtrack func(index int, curComb string)
 
-	backtrack = func(index int, digits, comb string) {
-		if index == len(digits) {
-			res = append(res, comb)
+	backtrack = func(index int, curComb string) {
+		if len(curComb) == len(digits) {
+			res = append(res, curComb)
 			return
 		}
 
-		letters := ln[digits[index]]
+		digitLetters := digitToLetters[digits[index]]
 
-		for i := 0; i < len(letters); i++ {
-			backtrack(index+1, digits, comb+string(letters[i]))
+		for i := 0; i < len(digitLetters); i++ {
+			backtrack(index+1, curComb+digitLetters[i])
 		}
 	}
 
-	backtrack(0, digits, "")
+	backtrack(0, "")
 	return res
 }
