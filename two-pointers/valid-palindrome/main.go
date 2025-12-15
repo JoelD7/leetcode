3a772bf6c63375e436383406305a8dca925a99d7
JoelD7 <joeld200013@gmail.com>
2025-12-15 09:58:39 -0400

    [Valid palindrome] Refactor

--- two-pointers/valid-palindrome/main.go
+++ two-pointers/valid-palindrome/main.go
@@ -1,17 +1,67 @@
 package valid_palindrome
 
-import "strings"
+import (
+	"strings"
+)
 
 func isPalindrome(s string) bool {
-	s = normalize(s)
-	if s == "" {
+	if len(s) == 1 {
 		return true
 	}
 
 	l := 0
 	r := len(s) - 1
+	s = strings.ToLower(s)
+	alphaNumerics := map[byte]bool{
+		'a': true,
+		'b': true,
+		'c': true,
+		'd': true,
+		'e': true,
+		'f': true,
+		'g': true,
+		'h': true,
+		'i': true,
+		'j': true,
+		'k': true,
+		'l': true,
+		'm': true,
+		'n': true,
+		'o': true,
+		'p': true,
+		'q': true,
+		'r': true,
+		's': true,
+		't': true,
+		'u': true,
+		'v': true,
+		'w': true,
+		'x': true,
+		'y': true,
+		'z': true,
+		'0': true,
+		'1': true,
+		'2': true,
+		'3': true,
+		'4': true,
+		'5': true,
+		'6': true,
+		'7': true,
+		'8': true,
+		'9': true,
+	}
 
 	for l < r {
+		if !alphaNumerics[s[l]] {
+			l++
+			continue
+		}
+
+		if !alphaNumerics[s[r]] {
+			r--
+			continue
+		}
+
 		if s[l] != s[r] {
 			return false
 		}
@@ -21,18 +71,4 @@ func isPalindrome(s string) bool {
 	}
 
 	return true
-}
-
-func normalize(s string) string {
-	s = strings.ToLower(s)
-
-	var result string
-
-	for _, c := range s {
-		if ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c >= '0' && c <= '9') {
-			result += string(c)
-		}
-	}
-
-	return result
 }
--- two-pointers/valid-palindrome/main_test.go
+++ two-pointers/valid-palindrome/main_test.go
@@ -12,11 +12,3 @@ func TestIsPalindrome(t *testing.T) {
 	c.False(isPalindrome("op"))
 	c.True(isPalindrome("A man, a plan, a canal: Panama"))
 }
-
-func TestNormalize(t *testing.T) {
-	c := require.New(t)
-
-	c.Equal("amanaplanacanalpanama", normalize("A man, a plan, a canal: Panama"))
-	c.Equal("raceacar", normalize("race a car"))
-	c.Equal("abrigo", normalize("?4ab8r)-i@#g== 0o"))
-}
