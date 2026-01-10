908e11e9d045256ab08da72fd0ab6933430f304b
JoelD7 <joeld200013@gmail.com>
2026-01-10 11:52:43 -0400

    [Design circular queue] Remove unnecessary comments

--- queue/design-circular-queue/main_test.go
+++ queue/design-circular-queue/main_test.go
@@ -8,20 +8,17 @@ import (
 func TestQueue(t *testing.T) {
 	t.Run("Example 1", func(t *testing.T) {
 		cQueue := Constructor(3)
-		assert.True(t, cQueue.EnQueue(1))  // return True
-		assert.True(t, cQueue.EnQueue(2))  // return True
-		assert.True(t, cQueue.EnQueue(3))  // return True
-		assert.False(t, cQueue.EnQueue(4)) // return False
-		assert.Equal(t, 3, cQueue.Rear())  // return 3
-		assert.True(t, cQueue.IsFull())    // return True
-		assert.True(t, cQueue.DeQueue())   // return True
-		assert.True(t, cQueue.EnQueue(4))  // return True
-		assert.Equal(t, 4, cQueue.Rear())  // return 4
+		assert.True(t, cQueue.EnQueue(1))
+		assert.True(t, cQueue.EnQueue(2))
+		assert.True(t, cQueue.EnQueue(3))
+		assert.False(t, cQueue.EnQueue(4))
+		assert.Equal(t, 3, cQueue.Rear())
+		assert.True(t, cQueue.IsFull())
+		assert.True(t, cQueue.DeQueue())
+		assert.True(t, cQueue.EnQueue(4))
+		assert.Equal(t, 4, cQueue.Rear())
 	})
 
-	//["MyCircularQueue","enQueue","Rear","Front","deQueue","Front","deQueue"."Front", "enQueue","enQueue","enQueue","enQueue"]
-	//[[3],[2],[],[],[],[],[],[],[4],[2],[2],[3]]
-	//[null,true,2,2,true ,-1,false,-1, true,true,true,false]
 	t.Run("Test case 2", func(t *testing.T) {
 		cQueue := Constructor(3)
 		assert.True(t, cQueue.EnQueue(2))
