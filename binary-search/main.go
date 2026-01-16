18f91296f048a418a58fded2aa53c6a8780a0cda
JoelD7 <joeld200013@gmail.com>
2026-01-16 10:46:47 -0400

    [Binary search] Solve

---
+++ binary-search/main.go
@@ -0,0 +1,22 @@
+package binary_search
+
+func search(nums []int, target int) int {
+	min := 0
+	max := len(nums) - 1
+
+	for min <= max {
+		middle := min + (max-min)/2
+
+		if nums[middle] == target {
+			return middle
+		}
+
+		if nums[middle] > target {
+			max = middle - 1
+		} else {
+			min = middle + 1
+		}
+	}
+
+	return -1
+}
