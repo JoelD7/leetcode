773052f545211aea7f4f51e834e23498af017e49
JoelD7 <joeld200013@gmail.com>
2026-02-05 10:30:12 -0400

    [Sliding Window Maximum] Decreasing monotonic queue, pruning elements outside the window range
    Passed 38/52 test cases. Got TLE errors

--- arrays/sliding-window/sliding-window-maximum/main.go
+++ arrays/sliding-window/sliding-window-maximum/main.go
@@ -1,32 +1,35 @@
 package main
 
-import (
-	"math"
-)
-
-type Queue struct {
+type Deque struct {
 	head *Node
 	tail *Node
 	size int
 }
 
 type Node struct {
-	val  int
+	val  Item
 	next *Node
+	prev *Node
 }
 
-func NewQueue() Queue {
-	return Queue{}
+type Item struct {
+	val   int
+	index int
 }
 
-func (this *Queue) isEmpty() bool {
+func NewQueue() Deque {
+	return Deque{}
+}
+
+func (this *Deque) isEmpty() bool {
 	return this.size == 0
 }
 
-func (this *Queue) Enqueue(x int) {
+func (this *Deque) PushBack(x Item) {
 	oldTail := this.tail
 	this.tail = &Node{
-		val: x,
+		val:  x,
+		prev: oldTail,
 	}
 
 	if this.isEmpty() {
@@ -38,44 +41,77 @@ func (this *Queue) Enqueue(x int) {
 	this.size++
 }
 
-func (this *Queue) Dequeue() int {
+func (this *Deque) PopFront() Item {
 	if this.isEmpty() {
-		return -1
+		return Item{}
 	}
 
 	val := this.head.val
 	this.head = this.head.next
+	if this.head != nil {
+		this.head.prev = nil
+	}
+
 	this.size--
 
 	return val
 }
 
-func (this *Queue) Peek() int {
+func (this *Deque) PopBack() Item {
+	if this.isEmpty() {
+		return Item{}
+	}
+
+	val := this.tail.val
+	this.tail = this.tail.prev
+	if this.tail != nil {
+		this.tail.next = nil
+	}
+	this.size--
+
+	return val
+}
+
+func (this *Deque) Front() Item {
 	return this.head.val
+}
+
+func (this *Deque) Back() Item {
+	return this.tail.val
 }
 
 func maxSlidingWindow(nums []int, k int) []int {
 	results := make([]int, 0)
-	max := math.MinInt32
-
 	queue := NewQueue()
+	var l, r int
+	var cur Item
 
-	for i := 0; i < k; i++ {
-		queue.Enqueue(nums[i])
+	for i := 0; i <= len(nums)-k; i++ {
+		l = i
+		r = i + k
+		for j := l; j < r; j++ {
+			cur = Item{
+				val:   nums[j],
+				index: j,
+			}
 
-		if nums[i] > max {
-			max = nums[i]
+			if queue.isEmpty() {
+				queue.PushBack(cur)
+				continue
+			}
+
+			for !queue.isEmpty() && queue.Back().val < cur.val {
+				queue.PopBack()
+			}
+
+			queue.PushBack(cur)
 		}
-	}
-	results = append(results, max)
 
-	for i := k; i < len(nums); i++ {
-		queue.Dequeue()
-		queue.Enqueue(nums[i])
-		if nums[i] > max {
-			max = nums[i]
+		for queue.Front().index < l {
+			queue.PopFront()
 		}
-		results = append(results, max)
+
+		results = append(results, queue.PopFront().val)
 	}
 
 	return results
--- arrays/sliding-window/sliding-window-maximum/main_test.go
+++ arrays/sliding-window/sliding-window-maximum/main_test.go
@@ -7,11 +7,16 @@ import (
 )
 
 func TestMaxSlidingWindow(t *testing.T) {
-	//Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
-	//	Output: [3,3,5,5,6,7]
-
 	t.Run("nums = [1,3,-1,-3,5,3,6,7], k = 3", func(t *testing.T) {
 		assert.Equal(t, []int{3, 3, 5, 5, 6, 7}, maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
+	})
+
+	t.Run("nums = [1,3,1,2,0,5], k = 3", func(t *testing.T) {
+		assert.Equal(t, []int{3, 3, 2, 5}, maxSlidingWindow([]int{1, 3, 1, 2, 0, 5}, 3))
+	})
+
+	t.Run("nums = [9,10,9,-7,-4,-8,2,-6], k = 5", func(t *testing.T) {
+		assert.Equal(t, []int{10, 10, 9, 2}, maxSlidingWindow([]int{9, 10, 9, -7, -4, -8, 2, -6}, 5))
 	})
 
 	t.Run("nums = 1, k = 1", func(t *testing.T) {
