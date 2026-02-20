54d4fdc9e04786c15080efa354f4d8b48e766baa
JoelD7 <joeld200013@gmail.com>
2026-02-20 10:24:54 -0400

    [Merge Two Sorted Lists] Solve

---
+++ linked-list/merge-two-sorted-lists/main.go
@@ -0,0 +1,60 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(val int) *ListNode {
+	return &ListNode{
+		Val: val,
+	}
+}
+
+func SetNext(val int, l *ListNode) *ListNode {
+	next := &ListNode{
+		Val: val,
+	}
+	l.Next = next
+	return next
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	var head *ListNode
+	if list1.Val < list2.Val {
+		head = list1
+		list1 = list1.Next
+	} else {
+		head = list2
+		list2 = list2.Next
+	}
+
+	cur := head
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
+
+	if list1 == nil {
+		cur.Next = list2
+	} else {
+		cur.Next = list1
+	}
+
+	return head
+}
