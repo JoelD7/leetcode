37aafc7f569a5e05b925fae4cedd10b0ef435c24
JoelD7 <joeld200013@gmail.com>
2026-02-25 10:04:24 -0400

    [Reorder List] Solve

---
+++ linked-list/reorder-list/README.md
@@ -0,0 +1,9 @@
+# Problem
+https://leetcode.com/problems/reorder-list/
+
+![img.png](img.png)![img_1.png](img_1.png)
+
+### Constraints:
+
+    The number of nodes in the list is in the range [1, 5 * 104].
+    1 <= Node.val <= 1000
---
+++ linked-list/reorder-list/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestReorderList(t *testing.T) {
+	head := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val:  4,
+					Next: nil,
+				},
+			},
+		},
+	}
+
+	reorderList(head)
+
+}
