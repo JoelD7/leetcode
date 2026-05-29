8c35ab0938e7fbbb63c6947947a656a80632df37
JoelD7 <joeld200013@gmail.com>
2026-05-29 10:47:41 -0400

    [Design Twitter] Optimized O(N) solution for GetNewsFeed function

--- heap/design-twitter/main.go
+++ heap/design-twitter/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"sort"
 	"time"
 )
 
 type Twitter struct {
-	m map[int]User
+	m map[int]*User
 }
 
 type User struct {
@@ -19,14 +18,85 @@ type Post struct {
 	timestamp int64
 }
 
-func Constructor() Twitter {
-	return Twitter{
-		m: make(map[int]User),
+type MinHeap struct {
+	arr []Post
+}
+
+func (h *MinHeap) size() int {
+	return len(h.arr)
+}
+
+func (h *MinHeap) insert(val Post) {
+	h.arr = append(h.arr, val)
+	h.heapifyUp(h.size() - 1)
+}
+
+func (h *MinHeap) heapifyUp(index int) {
+	parentIdx := parent(index)
+	for h.arr[parentIdx].timestamp > h.arr[index].timestamp {
+		h.swap(parentIdx, index)
+		index = parentIdx
+		parentIdx = parent(index)
 	}
 }
 
-func NewUser() User {
-	return User{
+func parent(index int) int {
+	return (index - 1) / 2
+}
+
+func leftChild(index int) int {
+	return 2*index + 1
+}
+
+func rightChild(index int) int {
+	return 2*index + 2
+}
+
+func (h *MinHeap) swap(i, j int) {
+	h.arr[i], h.arr[j] = h.arr[j], h.arr[i]
+}
+
+func (h *MinHeap) peek() Post {
+	return h.arr[0]
+}
+
+func (h *MinHeap) remove() Post {
+	val := h.arr[0]
+	h.arr[0] = h.arr[h.size()-1]
+	h.arr = h.arr[:h.size()-1]
+	h.heapifyDown(0)
+	return val
+}
+
+func (h *MinHeap) heapifyDown(index int) {
+	left, right := leftChild(index), rightChild(index)
+	minChild := 0
+
+	for left < h.size() {
+		if left == h.size()-1 || h.arr[left].timestamp < h.arr[right].timestamp {
+			minChild = left
+		} else {
+			minChild = right
+		}
+
+		if h.arr[minChild].timestamp < h.arr[index].timestamp {
+			h.swap(minChild, index)
+			index = minChild
+			left, right = leftChild(index), rightChild(index)
+		} else {
+			break
+		}
+	}
+}
+
+func Constructor() Twitter {
+	return Twitter{
+		m: make(map[int]*User),
+	}
+}
+
+func NewUser() *User {
+	return &User{
 		posts:   make([]Post, 0),
 		follows: make(map[int]bool),
 	}
@@ -48,28 +118,60 @@ func (this *Twitter) PostTweet(userId int, tweetId int) {
 	post := NewPost(tweetId)
 	user.posts = append(user.posts, post)
 
-	this.m[userId] = user
+	if !ok {
+		this.m[userId] = user
+	}
 }
 
 func (this *Twitter) GetNewsFeed(userId int) []int {
-	tweets := make([]int, 0)
 	user, _ := this.m[userId]
-	allPosts := user.posts
-
-	for followee, _ := range user.follows {
-		if len(this.m[followee].posts) < 10 {
-			allPosts = append(allPosts, this.m[followee].posts...)
-			continue
-		}
-		allPosts = append(allPosts, this.m[followee].posts[len(this.m[followee].posts)-10:]...)
+	if user == nil {
+		return []int{}
 	}
 
-	sort.Slice(allPosts, func(i, j int) bool {
-		return allPosts[i].timestamp > allPosts[j].timestamp
-	})
+	minHeap := &MinHeap{}
+	k := 10
 
-	for i := 0; i < 10 && i < len(allPosts); i++ {
-		tweets = append(tweets, allPosts[i].tweetID)
+	for _, post := range user.posts {
+		if minHeap.size() < k || post.timestamp > minHeap.peek().timestamp {
+			minHeap.insert(post)
+
+			if minHeap.size() > k {
+				minHeap.remove()
+			}
+		}
+	}
+
+	var post Post
+	var count int
+	var followee *User
+
+	//The inner loop will execute a maximum of k times, which is constant. So the complexity of these nested loops
+	//amortizes to O(N), where N = followees.
+	for followeeID, _ := range user.follows {
+		followee = this.m[followeeID]
+		if followee == nil {
+			continue
+		}
+
+		count = 0
+		for i := len(this.m[followeeID].posts) - 1; i >= 0 && count < k; i-- {
+			post = this.m[followeeID].posts[i]
+			count++
+
+			if minHeap.size() < k || post.timestamp > minHeap.peek().timestamp {
+				minHeap.insert(post)
+
+				if minHeap.size() > k {
+					minHeap.remove()
+				}
+			}
+		}
+	}
+
+	tweets := make([]int, minHeap.size())
+	for i := minHeap.size() - 1; i >= 0; i-- {
+		tweets[i] = minHeap.remove().tweetID
 	}
 
 	return tweets
--- heap/design-twitter/main_test.go
+++ heap/design-twitter/main_test.go
@@ -19,9 +19,6 @@ func TestTwitter(t *testing.T) {
 	})
 
 	t.Run("Test case 1", func(t *testing.T) {
-		//["Twitter","postTweet","getNewsFeed","follow","getNewsFeed","unfollow","getNewsFeed"]
-		//[[],[1,1],[1],[2,1],[2],[2,1],[2]]
-		//[null,null,[1],null,[1],null,[]]
 		tw := Constructor()
 		tw.PostTweet(1, 1)
 		assert.Equal(t, []int{1}, tw.GetNewsFeed(1))
@@ -29,5 +26,16 @@ func TestTwitter(t *testing.T) {
 		assert.Equal(t, []int{1}, tw.GetNewsFeed(2))
 		tw.Unfollow(2, 1)
 		assert.Equal(t, []int{}, tw.GetNewsFeed(2))
+	})
+
+	t.Run("Test case 2", func(t *testing.T) {
+		tw := Constructor()
+		assert.Empty(t, tw.GetNewsFeed(1))
+	})
+
+	t.Run("Test case 3", func(t *testing.T) {
+		tw := Constructor()
+		tw.Follow(1, 5)
+		assert.Empty(t, tw.GetNewsFeed(1))
 	})
 }
