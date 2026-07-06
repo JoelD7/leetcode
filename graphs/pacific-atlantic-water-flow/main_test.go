234780f2e65bdd249a8ab6b856e0c0beceacfcb6
JoelD7 <59536843+JoelD7@users.noreply.github.com>
2026-07-06 10:58:48 -0400

    [Pacific Atlantic Water Flow] Solve (#103)

---
+++ graphs/pacific-atlantic-water-flow/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPacificAtlantic(t *testing.T) {
+	t.Run("Example 1", func(t *testing.T) {
+		heights := [][]int{{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}}
+		expected := [][]int{{0, 4}, {1, 3}, {1, 4}, {2, 2}, {3, 0}, {3, 1}, {4, 0}}
+		assert.ElementsMatch(t, expected, pacificAtlantic(heights))
+	})
+}
