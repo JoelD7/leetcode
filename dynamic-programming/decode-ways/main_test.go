e17e2bed2df164a09e38eadc7b495c6abccc125c
JoelD7 <joeld200013@gmail.com>
2026-09-01 09:17:11 -0400

    [Decode Ways] Improve unit tests

--- dynamic-programming/decode-ways/main_test.go
+++ dynamic-programming/decode-ways/main_test.go
@@ -30,12 +30,4 @@ func TestNumDecodings(t *testing.T) {
 	t.Run("single digit: 1", func(t *testing.T) {
 		assert.Equal(t, 1, numDecodings("1"))
 	})
-
-	t.Run("long string with double digits", func(t *testing.T) {
-		assert.Equal(t, 2, numDecodings("111"))
-	})
-
-	t.Run("complex case with 0s", func(t *testing.T) {
-		assert.Equal(t, 0, numDecodings("2010"))
-	})
 }
